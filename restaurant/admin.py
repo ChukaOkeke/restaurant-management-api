4f790d3f0c73980ab86a64f8424d33164946038a
ChukaOkeke <chukaookeke@gmail.com>
2025-12-23 20:58:28 +0100

    configured and designed mysql database

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+admin.site.register(Menu) # Registering the Menu model
+admin.site.register(Booking) # Registering the Booking model
